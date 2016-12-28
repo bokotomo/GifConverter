48f3b1242ed4d5fc0d948fa272206567abc99cb1
tomonobu <bokotomo@me.com>
2016-12-28 23:56:36 +0900

    edit

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja"> 
+<head>
+<meta charset="utf-8">
+<title>Title</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width">
+<link rel='stylesheet' href='design.css'>
+<script src="js/jquery-2.0.2.min.js"></script>
+<script type="text/javascript">
+function uploadMovie(){
+  var upload_movie_form = document.getElementById("upload_movie_form");
+  upload_movie_form.click();
+  console.log(upload_movie_form);
+  postdata = "data=1";
+  AjaxProc("https://tomo.syo.tokyo/gifconverter/cgi/convert_movie.php",postdata,{
+    
+  });
+}
+
+function AjaxProc(url, postdata, callback){
+  //alert(postdata);
+	$.ajax({
+		url: url,
+		type: 'POST',
+		data: postdata,
+		success: function(data) {
+			alert(data);
+			data = JSON.parse(data);
+			callback(data);
+		}
+	});
+}
+</script>
+</head>
+<body>
+<div id="page">
+
+<div style="background: #fff;box-shadow: 0px 0px 2px 0px #888;width:800px;margin:auto;padding:50px 0px;border-radius: 5px;">
+  <form>
+    <input type="file" id="upload_movie_form" style="display: none;">
+  </form>
+  <div style="text-align: center;height:35px;line-height:35px;width:120px;margin:auto;border-radius: 5px;background: #f11717;color:#fff;cursor: pointer;" onclick="uploadMovie();">Upload</div>
+</div>
+
+</div>
+</body>
+</html>
