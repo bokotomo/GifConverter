ee5aa985e207cff98fd7d4ad92ef1d4e3a00eecc
tomonobu <bokotomo@me.com>
2017-01-03 01:00:20 +0900

    edit className and add function

--- cgi/convert_movie.php
+++ cgi/convert_movie.php
@@ -1,19 +1,25 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-if($_FILES["file"]["tmp_name"]){
-  list($fileName, $fileType) = explode(".",$_FILES['file']['name']);
-  checkFileType($fileType);
-  $fileFullName = date("YmdHis").".".$fileType;
-  $fileDir = __DIR__."/../video";
-  if (move_uploaded_file($_FILES['file']['tmp_name'], "{$fileDir}/{$fileFullName}")) {
-    chmod("{$fileDir}/{$fileFullName}", 0644);
-    //system("sh run.sh ../video/{$fileName}");
+run();
+
+function run(){
+  if($_FILES["file"]["tmp_name"]){
+    list($fileName, $fileType) = explode(".", $_FILES['file']['name']);
+    checkFileType($fileType);
+    $fileFullName = date("YmdHis").".{$fileType}";
+    $fileDir = __DIR__."/../video";
+    if (move_uploaded_file($_FILES['file']['tmp_name'], "{$fileDir}/{$fileFullName}")) {
+      chmod("{$fileDir}/{$fileFullName}", 0644);
+      $runFile = __DIR__."/run.sh";
+      $videoFile = __DIR__."/../video/{$fileName}";
+      system("sh {$runFile} {$videoFile}");
+    }
   }
 }
 
 function checkFileType($fileType){
-  if($fileType != "mov"){
+  if($fileType != "mov" && $fileType != "mp4"){
     exit(1);
   }
 }
